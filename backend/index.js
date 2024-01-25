6d70457acd39a8c88d1df05c2a6705cbc484569e
OskarPodrucki <oskar.podrucki1@gmail.com>
2024-01-25 19:40:07 +0100

    ...

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-app.use(cors())
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const app = express();
 
-const port = 3000
+const port = 3000;
 
-console.log("nosiema")
+app.use(
+	cookieSession({
+		name: "session",
+		keys: ["nosiema"],
+		maxAge: 24 * 60 * 60 * 1000,
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
 
 app.listen(port, () => {
-    console.log("Server is running. Port: " + port)
-})
+	console.log("Server is running. Port: " + port);
+});
